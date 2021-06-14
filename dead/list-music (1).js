0fa15021913694efbde62e152b762f64dc9a620f
Huy Tran <54362170+huytran-cloud@users.noreply.github.com>
2021-06-15 01:24:33 +0700

    Add files via upload

---
+++ dead/list-music (1).js
@@ -0,0 +1,3 @@
+function pia() {
+    document.body.style.backgroundImage="url('" +  + "')";
+}
---
+++ dead/music.js
@@ -0,0 +1,12 @@
+function button(){
+    location.assign("list-music (1).html"); //t sửa cái link vào trang 2 cho m rồi :))
+}
+
+
+$( document ).ready(function() { // function nhỏ nhỏ để chạy pop up siêu dễ :))
+    $('#headerVideoLink').magnificPopup({
+     type:'inline',
+     midClick: true // Cái này cho phép m ấn nút giữa của chuột vẫn chạy đc video... cứ để true nếu m không muốn động vào nó 
+   });
+     
+   });
