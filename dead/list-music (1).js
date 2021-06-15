d510ccdd293255f9cbfc5c738e6ffd781407ee56
Huy Tran <54362170+huytran-cloud@users.noreply.github.com>
2021-06-15 13:44:42 +0700

    Change file content

--- dead/list-music (1).js
+++ dead/list-music (1).js
@@ -1,3 +1,12 @@
 function pia() {
     document.body.style.backgroundImage="url('" +  + "')";
 }
+
+
+$( document ).ready(function() { // function nhỏ nhỏ để chạy pop up siêu dễ :))
+    $('#headerVideoLink').magnificPopup({
+     type:'inline',
+     midClick: true // Cái này cho phép m ấn nút giữa của chuột vẫn chạy đc video... cứ để true nếu m không muốn động vào nó 
+   });
+     
+   });
